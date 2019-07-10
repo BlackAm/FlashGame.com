8ee62925b6523bf19a687ba7b42a82cfc07373f3
BlackAm <41035708+BlackAm@users.noreply.github.com>
2019-07-10 18:08:44 +0900

    check

---
+++ FlashGame.com/public/gametable.js
@@ -0,0 +1,20 @@
+
+function BigDadAndMe() {        
+    document.getElementById('DadAndMe').style = 'width: 100%; height: 100%;'
+    document.getElementById('card card-4-DadAndMe').style = 'width: 800px; height: 800px;'
+    document.getElementById('DadAndMeButton').style = 'opacity: 0.9;'
+} function SmallDadAndMe() {
+    document.getElementById('DadAndMe').style = 'width: 100%; height: 100%;'
+    document.getElementById('card card-4-DadAndMe').style = ''
+    document.getElementById('DadAndMeButton').style = 'opacity: 0;'
+}
+
+function BigForestTemple() {        
+    document.getElementById('ForestTemple').style = 'width: 100%; height: 100%;'
+    document.getElementById('card card-4-ForestTemple').style = 'width: 800px; height: 800px;'
+    document.getElementById('ForestTempleButton').style = 'opacity: 0.9;'
+} function SmallForestTemple() {
+    document.getElementById('ForestTemple').style = 'width: 100%; height: 100%;'
+    document.getElementById('card card-4-ForestTemple').style = ''
+    document.getElementById('ForestTempleButton').style = 'opacity: 0;'
+}
